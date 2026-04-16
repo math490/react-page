7d771792b5177473f5e57bc4c157561d296bdb59
Marcos Teles Andrade <m.teles@estudante.ifro.edu.br>
2026-04-16 14:55:28 -0400

    Feat: capa

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
-import './App.css'
+import './App.css';
+import Capa from "./Capa.jsx"
+import brasCubasImage from './assets/bras_cubas.jpeg';
 
 function App() {
 
+  const informacoesLivro = {
+    nome: 'Memórias Póstumas de Brás Cubas',
+    autor: 'Machado de Assis',
+    totalCapitulos: 2,
+    capa: brasCubasImage,
+    textoAlternativo: 'Capa do livro Memórias Póstumas de Brás Cubas.'
+  }
+
   return (
     <>
-      <h1>Hello, World!</h1>
+      <Capa 
+      imagemCapa={informacoesLivro.capa}
+      textoAlternativo={informacoesLivro.textoAlternativo}
+      />
     </>
   )
 }
