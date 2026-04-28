9c54efed5ae25d98dc071f924f735373f3a63222
Matheus Teles de Andrade <telesm294@gmail.com>
2026-04-27 21:11:29 -0400

    Update: /src/assets/capitulos/

--- src/assets/capitulos/livro.js
+++ src/assets/capitulos/livro.js
@@ -1,6 +1,69 @@
 import faixa1 from "./01.mp3";
 import faixa2 from "./02.mp3";
+import faixa3 from "./03.mp3";
+import faixa4 from "./04.mp3";
+import faixa5 from "./05.mp3";
+import faixa6 from "./06.mp3";
+import faixa7 from "./07.mp3";
+import faixa8 from "./08.mp3";
+import faixa9 from "./09.mp3";
+import faixa10 from "./10.mp3";
+import faixa11 from "./11.mp3";
+import faixa12 from "./12.mp3";
+import faixa13 from "./13.mp3";
+import faixa14 from "./14.mp3";
+import faixa15 from "./15.mp3";
+import faixa16 from "./16.mp3";
+import faixa17 from "./17.mp3";
+import faixa18 from "./18.mp3";
+import faixa19 from "./19.mp3";
+import faixa20 from "./20.mp3";
+import faixa21 from "./21.mp3";
+import faixa22 from "./22.mp3";
+import faixa23 from "./23.mp3";
+import faixa24 from "./24.mp3";
+import faixa25 from "./25.mp3";
+import faixa26 from "./26.mp3";
+import faixa27 from "./27.mp3";
+import faixa28 from "./28.mp3";
+import faixa29 from "./29.mp3";
+import faixa30 from "./30.mp3";
+import faixa31 from "./31.mp3";
+import faixa32 from "./32.mp3";
 
-const livro = [faixa1, faixa2];
+const livro = [
+    faixa1,
+    faixa2,
+    faixa3,
+    faixa4,
+    faixa5,
+    faixa6,
+    faixa7,
+    faixa8,
+    faixa9,
+    faixa10,
+    faixa11,
+    faixa12,
+    faixa13,
+    faixa14,
+    faixa15,
+    faixa16,
+    faixa17,
+    faixa18,
+    faixa19,
+    faixa20,
+    faixa21,
+    faixa22,
+    faixa23,
+    faixa24,
+    faixa25,
+    faixa26,
+    faixa27,
+    faixa28,
+    faixa29,
+    faixa30,
+    faixa31,
+    faixa32,
+];
 
 export default livro;
